b14f92562022b0ce9c0b089f33f1b7ec282b01ea
Seth Troisi <sethtroisi@google.com>
2020-10-15 20:19:10 -0700

    Fix licenses

--- sieve_util.hpp
+++ sieve_util.hpp
@@ -1,19 +1,16 @@
-/* large_sieve.cpp
- * $ g++ -O2 script/large_sieve.cpp -o bin/large_sieve -lm -lgmp
- * $ time bin/large_sieve 77803 10709 46410 -216906 263222 > tmp/test.txt
- 2*
- * Author: Seth Troisi
- * Modified: 2020
- *
- * Take input gap (m, p, d, a, gap)
- *      start = (m * p#/d) -a
- *      end   = (m * p#/d) -a + gap
- *
- * Sieve up to some reasonable bound [1]
- * print to stdout numbers that need to be checked
- *
- * [1] See math in next_prime.c in gmp-lib (by Seth)
- */
+// Copyright 2020 Seth Troisi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #pragma once
 
